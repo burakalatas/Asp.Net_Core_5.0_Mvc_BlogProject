672032d6a0323c0cc536def5f1c9b58c6463f580
Burak Alatas <burak.alatas2011@gmail.com>
2022-07-03 20:04:48 +0300

    Add project files.

---
+++ EntityLayer/Concrete/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public string CategoryDescription { get; set; }
+        public bool CategoryStatus { get; set; }
+        public List<Blog> Blogs { get; set; }
+    }
+}
