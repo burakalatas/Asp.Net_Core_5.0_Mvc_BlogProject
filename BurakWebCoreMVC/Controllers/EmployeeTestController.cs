2c6bf16d95e6837d53dda5150f0d4317eae1030b
Burak337 <burak.alatas2011@gmail.com>
2022-10-30 14:48:46 +0300

    Some Employee Stuff

--- BurakWebCoreMVC/Controllers/EmployeeTestController.cs
+++ BurakWebCoreMVC/Controllers/EmployeeTestController.cs
@@ -2,6 +2,7 @@
 using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace BurakWebCoreMVC.Controllers
@@ -11,10 +12,28 @@ namespace BurakWebCoreMVC.Controllers
         public async Task<IActionResult> Index()
         {
             var httpClient = new HttpClient();
-            var responseMessage = await httpClient.GetAsync("");
+            var responseMessage = await httpClient.GetAsync("https://localhost:44397/api/Default");
             var jsonString = await responseMessage.Content.ReadAsStringAsync();
             var values = JsonConvert.DeserializeObject<List<Class1>>(jsonString);
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddEmployee()
+        {
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddEmployee(Class1 p)
+        {
+            var httpClient = new HttpClient();
+            var jsonEmployee = JsonConvert.SerializeObject(p);
+            StringContent content = new StringContent(jsonEmployee,Encoding.UTF8,"application/json");
+            var responseMessage = await httpClient.PostAsync("https://localhost:44397/api/Default", content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(p);
         }
     }
     public class Class1
